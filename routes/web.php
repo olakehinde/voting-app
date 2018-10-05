20c2c06e9d1410df8a12c5963e78bde4049a0104
olakehinde <olakehinde59@gmail.com>
2018-10-05 13:33:10 +0100

    added the registration route to redirect to the facebook login method

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('login/facebook', 'Auth\LoginController@redirectToProvider')->name('login.facebook');
 Route::get('login/facebook/callback', 'Auth\LoginController@handleProviderCallback');
+Route::get('register', 'Auth\LoginController@redirectToProvider')->name('register');
