42cbafd4df8648d7f091f938f058b131109bf0ba
olakehinde <olakehinde59@gmail.com>
2018-10-05 14:08:54 +0100

    created the nominations migration for nomination model/table

---
+++ database/migrations/2018_10_05_124324_create_nominations_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNominationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nominations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('bio')->nullable();
+            $table->string('gender');
+            $table->string('occupation')->nullable();
+            $table->string('reasons_for_nomination')->nullable();
+            $table->integer('no_of_nominations');
+            $table->integer('category_id');
+            $table->integer('user_id');
+            $table->boolean('is_won')->default(0);
+            $table->boolean('is_admin_selected')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nominations');
+    }
+}
