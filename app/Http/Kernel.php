66eb5524651b24fbc1d052c06f74cdda361a4343
olakehinde <olakehinde59@gmail.com>
2018-10-06 17:08:53 +0100

    registered the newly created middlewares in the kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+        'moderator' => \App\Http\Middleware\Moderator::class,
+        'auditfirm' => \App\Http\Middleware\AuditFirm::class,
     ];
 }
