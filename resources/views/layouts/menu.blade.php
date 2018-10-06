f3a5d02c621c1fcfb488a5f7f0d12c4632dc4ab7
olakehinde <olakehinde59@gmail.com>
2018-10-06 16:21:07 +0100

    fixed the user access roles features

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -6,19 +6,26 @@
     <a href="{!! route('nominations.index') !!}"><i class="fa fa-edit"></i><span>Nominations</span></a>
 </li>
 
-<li class="{{ Request::is('roles*') ? 'active' : '' }}">
-    <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
-</li>
-
-<li class="{{ Request::is('settings*') ? 'active' : '' }}">
-    <a href="{!! route('settings.index') !!}"><i class="fa fa-edit"></i><span>Settings</span></a>
-</li>
-
-<li class="{{ Request::is('users*') ? 'active' : '' }}">
-    <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
-</li>
-
 <li class="{{ Request::is('votings*') ? 'active' : '' }}">
     <a href="{!! route('votings.index') !!}"><i class="fa fa-edit"></i><span>Votings</span></a>
-</li>
+</li> 
+
+@if(Auth::user()->role_id < 3)
+	<li class="{{ Request::is('users*') ? 'active' : '' }}">
+	    <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
+	</li>
+
+	@if(Auth::user()->role_id == 1)
+	<li class="{{ Request::is('roles*') ? 'active' : '' }}">
+	    <a href="{!! route('roles.index') !!}"><i class="fa fa-edit"></i><span>Roles</span></a>
+	</li>
+
+	<li class="{{ Request::is('settings*') ? 'active' : '' }}">
+	    <a href="{!! route('settings.index') !!}"><i class="fa fa-edit"></i><span>Settings</span></a>
+	</li>
+	@endif
+@endif
+
+
+
 
