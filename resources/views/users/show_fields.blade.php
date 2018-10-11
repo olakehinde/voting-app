049ad73243c4ab3bf3eac23e3380a55be956d028
olakehinde <olakehinde59@gmail.com>
2018-10-11 20:12:31 +0100

    fixed the user profile page. finished the design of the user profile view page

--- resources/views/users/show_fields.blade.php
+++ resources/views/users/show_fields.blade.php
@@ -1,48 +1,35 @@
-<!-- Id Field -->
-<div class="form-group">
-    {!! Form::label('id', 'Id:') !!}
-    <p>{!! $user->id !!}</p>
-</div>
+<div class="col-md-6 offset-3">
+    <div class="box box-widget widget-user">
+        <!-- Add the bg color to the header using any of the bg-* classes -->
+        <div class="widget-user-header bg-aqua-active">
+          <h3 class="widget-user-username">{{$user->name}}</h3>
+          <h5 class="widget-user-desc">{{$user->email}}</h5>
+        </div>
 
-<!-- Name Field -->
-<div class="form-group">
-    {!! Form::label('name', 'Name:') !!}
-    <p>{!! $user->name !!}</p>
-</div>
+        <div class="widget-user-image">
+          <img class="img-circle" src="{{$user->avatar}}" alt="{{$user->name}}">
+        </div>
 
-<!-- Email Field -->
-<div class="form-group">
-    {!! Form::label('email', 'Email:') !!}
-    <p>{!! $user->email !!}</p>
-</div>
+        <div class="box-footer">
+            <div class="row">
+                <div class="col-sm-12 border-right">
+                  <div class="description-block">
+                    <h5 class="description-header">Role</h5>
+                    <span class="description-text">{{$user->role['name']}}</span>
+                  </div>
+                  <!-- /.description-block -->
+                </div>
+            </div>
+            <!-- /.row -->
 
-<!-- Password Field -->
-<div class="form-group">
-    {!! Form::label('password', 'Password:') !!}
-    <p>{!! $user->password !!}</p>
+            <div class="box-footer no-padding">
+              <ul class="nav nav-stacked">
+                <li><a href="#">Projects <span class="pull-right badge bg-blue">31</span></a></li>
+                <li><a href="#">Tasks <span class="pull-right badge bg-aqua">5</span></a></li>
+                <li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a></li>
+                <li> Joined <span class="pull-right">{{$user->created_at->format('M, Y') }}</span></li>
+              </ul>
+            </div>
+        </div>
+    </div>
 </div>
-
-<!-- Avatar Field -->
-<div class="form-group">
-    {!! Form::label('avatar', 'Avatar:') !!}
-    <p>{!! $user->avatar !!}</p>
-</div>
-
-<!-- Remember Token Field -->
-<div class="form-group">
-    {!! Form::label('remember_token', 'Remember Token:') !!}
-    <p>{!! $user->remember_token !!}</p>
-</div>
-
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{!! $user->created_at !!}</p>
-</div>
-
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{!! $user->updated_at !!}</p>
-</div>
-
