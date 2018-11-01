464780751a805e0371259a0954cc81353f83b0c4
olakehinde <olakehinde59@gmail.com>
2018-11-01 09:31:13 +0100

    redesigned the settings form fields for nomination and voting date settings using the datetimepicker library

--- resources/views/settings/fields.blade.php
+++ resources/views/settings/fields.blade.php
@@ -1,65 +1,51 @@
-<div class="container">
-    <form action="" class="form-horizontal"  role="form">
-        <fieldset>
-            <legend>Test</legend>
-            <div class="form-group">
-                <label for="dtp_input1" class="col-md-2 control-label">DateTime Picking</label>
-                <div class="input-group date form_datetime col-md-5" data-date="1979-09-16T05:25:07Z" data-date-format="dd MM yyyy - HH:ii p" data-link-field="dtp_input1">
-                    <input class="form-control" size="16" type="text" value="" readonly>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
-                </div>
-                <input type="hidden" id="dtp_input1" value="" /><br/>
-            </div>
-            <div class="form-group">
-                <label for="dtp_input2" class="col-md-2 control-label">Date Picking</label>
-                <div class="input-group date form_date col-md-5" data-date="" data-date-format="dd MM yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">
-                    <input class="form-control" size="16" type="text" value="" readonly>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-calendar"></span></span>
-                </div>
-                <input type="hidden" id="dtp_input2" value="" /><br/>
-            </div>
-            <div class="form-group">
-                <label for="dtp_input3" class="col-md-2 control-label">Time Picking</label>
-                <div class="input-group date form_time col-md-5" data-date="" data-date-format="hh:ii" data-link-field="dtp_input3" data-link-format="hh:ii">
-                    <input class="form-control" size="16" type="text" value="" readonly>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-time"></span></span>
-                </div>
-                <input type="hidden" id="dtp_input3" value="" /><br/>
-            </div>
-        </fieldset>
-    </form>
-</div>
+<div class="container-fluid">
+    <!-- Nomination Start Date Field -->
+    <div class="form-group col-sm-6">
+        <label for="nomination_start_date" class="col-md-3 control-label">Nomination Start Date:</label>
+        <div class="input-group date form_datetime col-md-9" data-date="1979-09-16T05:25:07Z" data-date-format="dd MM yyyy - HH:ii p" data-link-field="nomination_start_date">
+            <input class="form-control" size="16" type="text" value="" readonly name="nomination_start_date">
+            <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
+            <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
+        </div>
+        <input type="hidden" id="nomination_start_date" value="" /><br/>
+    </div>
 
+    <!-- Nomination End Date Field -->
+    <div class="form-group col-sm-6">
+        <label for="nomination_end_date" class="col-md-3 control-label">Nomination End Date:</label>
+        <div class="input-group date form_datetime col-md-9" data-date="1979-09-16T05:25:07Z" data-date-format="dd MM yyyy - HH:ii p" data-link-field="nomination_end_date">
+            <input class="form-control" size="16" type="text" value="" readonly name="nomination_end_date">
+            <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
+            <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
+        </div>
+        <input type="hidden" id="nomination_start_date" value="" /><br/>
+    </div>
 
-<!-- Nomination Start Date Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('nomination_start_date', 'Nomination Start Date:') !!}
-    {!! Form::date('nomination_start_date', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Voting Start Date Field -->
+    <div class="form-group col-sm-6">
+        <label for="voting_start_date" class="col-md-3 control-label">Voting Start Date:</label>
+        <div class="input-group date form_datetime col-md-9" data-date="1979-09-16T05:25:07Z" data-date-format="dd MM yyyy - HH:ii p" data-link-field="voting_start_date">
+            <input class="form-control" size="16" type="text" value="" readonly name="voting_start_date">
+            <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
+            <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
+        </div>
+        <input type="hidden" id="voting_start_date" value="" /><br/>
+    </div>
 
-<!-- Nomination End Date Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('nomination_end_date', 'Nomination End Date:') !!}
-    {!! Form::date('nomination_end_date', null, ['class' => 'form-control']) !!}
-</div>
+    <!-- Voting End Date Field -->
+    <div class="form-group col-sm-6">
+        <label for="voting_end_date" class="col-md-3 control-label">Voting End Date:</label>
+        <div class="input-group date form_datetime col-md-9" data-date="1979-09-16T05:25:07Z" data-date-format="dd MM yyyy - HH:ii p" data-link-field="voting_end_date">
+            <input class="form-control" size="16" type="text" value="" readonly name="voting_end_date">
+            <span class="input-group-addon"><span class="glyphicon glyphicon-remove"></span></span>
+            <span class="input-group-addon"><span class="glyphicon glyphicon-th"></span></span>
+        </div>
+        <input type="hidden" id="voting_end_date" value="" /><br/>
+    </div>
 
-<!-- Voting Start Date Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('voting_start_date', 'Voting Start Date:') !!}
-    {!! Form::date('voting_start_date', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Voting End Date Field -->
-<div class="form-group col-sm-6">
-    {!! Form::label('voting_end_date', 'Voting End Date:') !!}
-    {!! Form::date('voting_end_date', null, ['class' => 'form-control']) !!}
-</div>
-
-<!-- Submit Field -->
-<div class="form-group col-sm-12">
-    {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
-    <a href="{!! route('settings.index') !!}" class="btn btn-default">Cancel</a>
+    <!-- Submit Field -->
+    <div class="form-group pull-right">
+        {!! Form::submit('Save', ['class' => 'btn btn-success']) !!}
+        <a href="{!! route('settings.index') !!}" class="btn btn-danger">Cancel</a>
+    </div>
 </div>
