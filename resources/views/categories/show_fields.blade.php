79250aaed5d7df316b6a8406ad894dea61acb991
olakehinde <olakehinde59@gmail.com>
2018-11-26 14:27:47 +0100

    removed the section of the categories show page that is showing the category name

--- resources/views/categories/show_fields.blade.php
+++ resources/views/categories/show_fields.blade.php
@@ -1,33 +1,4 @@
-<div class="col-md-2">
-    <!-- Profile Image -->
-    <div class="box box-primary">
-        <div class="box-body box-profile">
-          
-            <h3 class="profile-username text-center">{!! $category->name !!}</h3>
-
-            <p class="text-muted text-center">Last Updated: {!! $category->updated_at->format('D, M d, Y') !!}</p>
-
-            <!-- <ul class="list-group list-group-unbordered">
-                <li class="list-group-item">
-                  <b>Followers</b> <a class="pull-right">1,322</a>
-                </li>
-                <li class="list-group-item">
-                  <b>Following</b> <a class="pull-right">543</a>
-                </li>
-                <li class="list-group-item">
-                  <b>Friends</b> <a class="pull-right">13,287</a>
-                </li>
-            </ul>
-
-            <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a> -->
-        </div>
-        <!-- /.box-body -->
-    </div>
-    <!-- /.box -->
-</div>
-<!-- /.col -->
-
-<div class="col-md-10">
+<div class="col-md-12">
     <div class="nav-tabs-custom">
         <ul class="nav nav-tabs">
 
