ad2895e17a05e4ef9ed8ff024a733c953eb3c1fb
olakehinde <olakehinde59@gmail.com>
2018-12-03 18:39:12 +0100

    implemented pagination in the show user table page

--- resources/views/users/table.blade.php
+++ resources/views/users/table.blade.php
@@ -1,46 +1,13 @@
-<table class="table table-responsive" id="users-table">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th colspan="3">Action</th>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach($users as $user)
-        <tr>
-            <td><strong><a href="{!! route('users.show', [$user->id]) !!}">{!! $user->name !!}</a></strong></td>
-            <td>{!! $user->email !!}</td>
-            <td>{!! $user->role['name'] !!}</td>
-            <td>
-                {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'delete']) !!}
-                <div class='btn-group'>
-                    <a href="{!! route('users.show', [$user->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    <a href="{!! route('users.edit', [$user->id]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                </div>
-                {!! Form::close() !!}
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
-
-
 <div class="box">
-    <div class="box-header">
-      <h3 class="box-title">Data Table With Full Features</h3>
-    </div>
     <!-- /.box-header -->
     <div class="box-body">
         <div id="example_wrapper" class="dataTables_wrapper form-inline dt-bootstrap">
             <div class="row">
-                <div class="col-sm-6">
-                    <div class="dataTables_length" id="example_length"><label>Show <select name="example_length" aria-controls="example" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label>
+                <div class="col-sm-6 pull-right">
+                    <div id="example_filter" class="dataTables_filter">
+                        <label>Search:<input type="search" class="form-control input-sm" aria-controls="example">
+                        </label>
                     </div>
-                </div>
-                <div class="col-sm-6"><div id="example_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="example"></label></div>
                 </div>
             </div>
 
@@ -64,6 +31,7 @@
                         </thead>
 
                         <tbody>
+                            @foreach($users as $user)
                             <tr role="row" class="odd">
                               <td class="sorting_1">{!! $user->name !!}</td>
                               <td>{!! $user->email !!}</td>
@@ -77,51 +45,22 @@
                                 </div>
                                 {!! Form::close() !!}
                               </td>
-                              <td>A</td>
                             </tr>
+                            @endforeach
                         </tbody>
 
                         <tfoot>
-                        <tr><th rowspan="1" colspan="1">Rendering engine</th><th rowspan="1" colspan="1">Browser</th><th rowspan="1" colspan="1">Platform(s)</th><th rowspan="1" colspan="1">Engine version</th><th rowspan="1" colspan="1">CSS grade</th></tr>
+                            <tr>
+                                <th rowspan="1" colspan="1">Rendering engine</th>
+                                <th rowspan="1" colspan="1">Browser</th>
+                                <th rowspan="1" colspan="1">Platform(s)</th>
+                                <th rowspan="1" colspan="1">Engine version</th>
+                            </tr>
                         </tfoot>
                     </table>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-sm-5">
-                    <div class="dataTables_info" id="example_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div>
-                </div>
-                <div class="col-sm-7">
-                    <div class="dataTables_paginate paging_simple_numbers" id="example_paginate">
-                        <ul class="pagination">
-                            <li class="paginate_button previous disabled" id="example_previous">
-                                <a href="#" aria-controls="example" data-dt-idx="0" tabindex="0">Previous</a>
-                            </li>
-                            <li class="paginate_button active">
-                                <a href="#" aria-controls="example" data-dt-idx="1" tabindex="0">1</a>
-                            </li>
-                            <li class="paginate_button">
-                                <a href="#" aria-controls="example" data-dt-idx="2" tabindex="0">2</a>
-                            </li>
-                            <li class="paginate_button">
-                                <a href="#" aria-controls="example" data-dt-idx="3" tabindex="0">3</a>
-                            </li>
-                            <li class="paginate_button">
-                                <a href="#" aria-controls="example" data-dt-idx="4" tabindex="0">4</a>
-                            </li>
-                            <li class="paginate_button">
-                                <a href="#" aria-controls="example" data-dt-idx="5" tabindex="0">5</a>
-                            </li>
-                            <li class="paginate_button">
-                                <a href="#" aria-controls="example" data-dt-idx="6" tabindex="0">6</a>
-                            </li>
-                            <li class="paginate_button next" id="example_next">
-                                <a href="#" aria-controls="example" data-dt-idx="7" tabindex="0">Next</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            {{$users->links()}}
         </div>
     </div>
     <!-- /.box-body -->
