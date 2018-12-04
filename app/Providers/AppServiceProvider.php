f910424992986c0661e33a3c8b6254821a69ab1b
olakehinde <olakehinde59@gmail.com>
2018-12-04 15:50:48 +0100

    implemented the logic to check if the current datetime is within the nomination and voting periods and shared the response with the views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,10 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use App\Models\Setting;
+use Carbon\Carbon;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -14,6 +18,28 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        $currentTime = Carbon::now();
+        
+        $setting = Setting::find(1);
+
+        if ($currentTime->gt($setting->nomination_start_date) && $currentTime->lt($setting->nomination_end_date) ) {
+            // returns true if current time is within the nomination period
+            View::share('isWithinNominationPeriod', 'YES');
+        }
+        else
+        {
+            View::share('isWithinNominationPeriod', 'NO');
+        }
+
+        if ($currentTime->gt($setting->voting_start_date) && $currentTime->lt($setting->voting_end_date) ) {
+            // returns true if current time is within the voting period
+            View::share('isWithinVotingPeriod', 'YES');
+        }
+        else
+        {
+            View::share('isWithinVotingPeriod', 'NO');
+        }
     }
 
     /**
