2b354e3c63d498ab6b554c380e8faad726ce8208
olakehinde <olakehinde59@gmail.com>
2018-12-04 15:55:38 +0100

    modified the login page to appropriately show the data according to the period. if the current time is within the nomination period, show nomination info. else if the current datetime is within voting period, show voting info. else, show voting and nomination info

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,12 +3,28 @@
 @section('content')
 <div class="login-box">
     <div class="login-logo">
-        <a href="{{ url('/home') }}"><b>Voting </b>App</a>
+        <a href="{{ url('/home') }}"><b>
+            @if($isWithinNominationPeriod == 'YES')
+                Nomination
+            @elseif($isWithinVotingPeriod == 'YES')
+                Voting
+            @else
+                Voting and Nomination
+            @endif
+            </b>App</a>
     </div>
 
     <!-- /.login-logo -->
     <div class="login-box-body">
-        <p class="login-box-msg">Login to vote</p>
+        <p class="login-box-msg">
+            @if($isWithinNominationPeriod == 'YES')
+                Login to Nominate a Candidate
+            @elseif($isWithinVotingPeriod == 'YES')
+                Login to Vote
+            @else
+                Nomination or Voting dates have not been set.
+            @endif
+        </p>
 
         <div class="social-auth-links text-center">
             <a href="login/facebook" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i>Login with Facebook</a>
