86ae2ddb2b538af46fec9df9e7fca9569b50eae8
olakehinde <olakehinde59@gmail.com>
2018-12-06 14:05:28 +0100

    modified the category show page to display the redesigned nomination card which now includes profile picture of the nominated candidate

--- resources/views/categories/show_fields.blade.php
+++ resources/views/categories/show_fields.blade.php
@@ -30,7 +30,7 @@
                 <!-- show nomination form if user has not nominated any candidate for this category before -->
                 @if(!isset($hasNominatedBefore) || $hasNominatedBefore === 0)
                     <div class="row">
-                        {!! Form::open(['route' => 'nominations.store']) !!}
+                        {!! Form::open(['route' => 'nominations.store', 'enctype' => 'multipart/form-data']) !!}
 
                             @include('nominations.fields')
 
@@ -47,12 +47,24 @@
                               <h5 class="widget-user-desc">{{$nominatedCandidate->occupation}}</h5>
                             </div>
 
+                            <div class="widget-user-image">
+                                <img class="img-circle" src="{{ asset('storage/uploads/images/'. $nominatedCandidate->id.'/'.$nominatedCandidate->image)}}" alt="{{$nominatedCandidate->name}}">
+                            </div>
+
                             <div class="box-footer">
                                 <div class="row">
-                                    <div class="col-sm-12 border-right">
+                                    <div class="col-sm-6 border-right">
+                                        <div class="description-block">
+                                        <h5 class="description-header">Gender</h5>
+                                        <span class="description-text">{{$nominatedCandidate->gender}}</span>
+                                        </div>
+                                        <!-- /.description-block -->
+                                    </div>
+
+                                    <div class="col-sm-6 border-right">
                                       <div class="description-block">
-                                        <h5 class="description-header">Bio</h5>
-                                        <span class="description-text">{{$nominatedCandidate->bio}}</span>
+                                        <h5 class="description-header">Nominations</h5>
+                                        <span class="description-text">{{$nominatedCandidate->no_of_nominations}}</span>
                                       </div>
                                       <!-- /.description-block -->
                                     </div>
@@ -61,10 +73,11 @@
 
                                 <div class="box-footer no-padding">
                                   <ul class="nav nav-stacked">
-                                    <li><a href="#">Projects <span class="pull-right badge bg-blue">31</span></a></li>
-                                    <li><a href="#">Tasks <span class="pull-right badge bg-aqua">5</span></a></li>
-                                    <li><a href="#">Completed Projects <span class="pull-right badge bg-green">12</span></a></li>
-                                    <li> Nominated <span class="pull-right">{{$nominatedCandidate->created_at->format('M, Y') }}</span></li>
+                                    <li><a href="#"><strong>Nominated</strong><span class="pull-right">{{$nominatedCandidate->created_at->format('M, Y') }}</span></a>
+                                    </li>
+                                    <li><a href="#"><strong>Bio</strong><span class="pull-right">{{$nominatedCandidate->bio }}</span></a></li>
+                                    <li><a href="#"><strong>Occupation</strong><span class="pull-right">{{$nominatedCandidate->occupation }}</span></a></li>
+                                    <li><a href="#"><strong>Why Nominate ?</strong><span class="pull-right">{{$nominatedCandidate->reasons_for_nomination }}</span></a></li>
                                   </ul>
                                 </div>
                             </div>
@@ -85,7 +98,7 @@
                 @endif
                 " id="nominees">
 
-                <!-- list of admin approved nominees -->
+                {{-- <!-- list of admin approved nominees -->
                 <h3> Nominees</h3>
 
                 @if(count($selectedNominations) > 0)
@@ -96,7 +109,7 @@
                     </div>
                 @else
                     <p>No Approved Nominee yet</p>
-                @endif
+                @endif --}}
 
                 @if(Auth::user()->role_id < 3)                
                 <!-- list of all nominees -->
