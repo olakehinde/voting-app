c0c2380addd5f627427e5401bf44d6bd06b975c3
olakehinde <olakehinde59@gmail.com>
2018-12-06 14:02:37 +0100

    added the file upload button field in the category field page

--- resources/views/categories/fields.blade.php
+++ resources/views/categories/fields.blade.php
@@ -4,6 +4,18 @@
     {!! Form::text('name', null, ['class' => 'form-control']) !!}
 </div>
 
+<!-- Icon name Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('icon', 'Name of Icon (optional): ') !!}
+    {!! Form::text('icon', null, ['class' => 'form-control']) !!}
+</div>
+
+<!-- Image Upload Field -->
+<div class="form-group col-sm-6">
+    {!! Form::label('image', 'Upload Category Image (optional): ') !!}
+    {!! Form::file('image', null, ['class' => 'form-control']) !!}
+</div>
+
 <!-- Submit Field -->
 <div class="form-group col-sm-12">
     {!! Form::submit('Save', ['class' => 'btn btn-primary']) !!}
