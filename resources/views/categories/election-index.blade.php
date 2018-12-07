991f3745f36efeda4e50b7fa5515e77cb94a51f5
olakehinde <olakehinde59@gmail.com>
2018-12-07 14:45:06 +0100

    created a new categories index page for voters

---
+++ resources/views/categories/election-index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.election-template')
+
+@section('content')
+    <!-- banner-bottom1 -->
+    <div class="banner-bottom1">
+        <div class="banner-bottom1-grids">
+            @foreach($categories as $category)
+            <a href="{!! route('categories.show', [$category->id]) !!}">
+                <div class="col-md-4 banner-bottom1-left banner-bottom1-left1">
+                    <div class="banner-bottom1-lft">
+                        <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                        <h3>{!! $category->name !!}</h3>
+                        <!-- <p>Lorem Ipsum is therefore always free.</p> -->
+                    </div>
+                </div>
+
+                {{--<div class="col-md-4 banner-bottom1-left">
+                    <div class="banner-bottom1-lft">
+                        <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
+                        <h3>Accessibility</h3>
+                        <p>Lorem Ipsum is therefore always free.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 banner-bottom1-left banner-bottom1-left2">
+                    <div class="banner-bottom1-lft">
+                        <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span>
+                        <h3>Calendar</h3>
+                        <p>Lorem Ipsum is therefore always free.</p>
+                    </div>
+                </div> --}}
+            </a>
+            @endforeach
+            <div class="clearfix"> </div>
+        </div>
+    </div>
+    <!-- //banner-bottom1 -->
+@endsection
+
